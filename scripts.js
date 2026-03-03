8feb423f209a6b79a037a0abe39681cbb331b7f0
Isaac Ziroldo <isaaclfr@gmail.com>
2026-03-03 19:32:13 -0300

    feat: extrai inclusão de despesa para função dedicada
    - substitui o console.log direto por chamada para expandAdd(newExpanse) no submit
    
    - adiciona função expandAdd com try/catch para tratamento de falhas
    
    - exibe alerta de sucesso e alerta de erro com log no console

--- scripts.js
+++ scripts.js
@@ -35,7 +35,15 @@ form.onsubmit = (event) => {
         amount: amount.value,
         created_at: new Date()
     }
+    expandAdd(newExpanse)
 
-    console.log(newExpanse)
+}
 
+function expandAdd(newExpanse) {
+    try {
+        alert("Despesa adcionada!!!")
+    } catch (error) {
+        alert("Não foi possível atualizar a lista de despesas.")
+        console.log(error)
+    }
 }
